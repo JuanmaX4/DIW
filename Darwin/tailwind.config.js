2d10b5ca9444ae8ecdeca2afccca1b849db7b515
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-02-02 19:07:08 +0100

    Update Final 1.0

--- Darwin/tailwind.config.js
+++ Darwin/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      symbols: ['WebSymbolsRegular'],
+      bebas: ['BebasNeue'],
+    },
       extend: {},
+  },
+  variants: {
+    animation: ['responsive', 'hover', 'group-hover'],
+    animate: ['responsive', 'hover', 'group-hover'],
+    fontSize: ['responsive', 'hover', 'group-hover'],
   },
   plugins: [],
 }
