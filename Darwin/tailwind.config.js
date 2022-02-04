1ff1d096f340b0227812d3fb9aa320d861b4c377
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-02-04 18:14:45 +0100

    Update Final 1.2.5.876512

--- Darwin/tailwind.config.js
+++ Darwin/tailwind.config.js
@@ -1,16 +1,32 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    fontFamily: {
-      symbols: ['WebSymbolsRegular'],
-      bebas: ['BebasNeue'],
-    },
-      extend: {},
-  },
   variants: {
-    animation: ['responsive', 'hover', 'group-hover'],
-    animate: ['responsive', 'hover', 'group-hover'],
-    fontSize: ['responsive', 'hover', 'group-hover'],
+    extend: {
+        animation: ['responsive', 'hover', 'group-hover'],
+        fontSize: ['responsive', 'hover', 'group-hover'],
+    }
   },
-  plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        symbols: ['WebSymbolsRegular'],
+        bebas: ['BebasNeue'],
+      },
+      animation: {
+        'engranaje-lento': 'spin 4s linear infinite',
+        'paAbajo': 'paAbajo 3s linear',
+        'palaDerecha': 'palaDerecha 3s linear'
+      },
+      keyframes: {
+        paAbajo: { 
+          '0%': { transfrom: 'translateY(0%)'},                     
+          '50%': { transform: 'transalateY(150%)'},                     
+          '100%': { transform: 'translateY(0%)'}                 
+        },                
+        palaDerecha: { 
+            '0%': { transform: 'transalateX(150%)'},
+            '100%': { transform: 'translateX(0%)'}
+        }
+      }
+    }
+  }
 }
