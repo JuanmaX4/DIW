68b367b190c829ed0c1cf753d30f828452969582
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-02-09 19:41:18 +0100

    Update Alpha 0.9.87651

---
+++ Proyecto individual Darwin/Darwin/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  variants: {
+    extend: {
+        animation: ['responsive', 'hover', 'group-hover'],
+        fontSize: ['responsive', 'hover', 'group-hover'],
+    }
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        symbols: ['WebSymbolsRegular'],
+        bebas: ['BebasNeue'],
+      },
+      animation: {
+        'barrita': 'barrita 3s linear'
+      },
+      keyframes: {
+        barrita: { 
+          '0%': { transform: 'scale(1) rotate(0deg)' },
+          '50%': { transform: 'scale(1.6) rotate(10deg)'}                  
+        },         
+      }
+    }
+  }
+}
